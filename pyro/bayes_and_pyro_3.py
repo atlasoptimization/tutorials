116575911b90c6ee575ea8f04a502a725611657c
atlasoptimization <jemilbutt.opt@gmail.com>
2024-02-22 14:36:36 +0100

    Add pyro inference to bayes_and_pyro_3.py

--- pyro/bayes_and_pyro_3.py
+++ pyro/bayes_and_pyro_3.py
@@ -120,84 +120,132 @@ product_likelihoods_given_mu = torch.prod(likelihood_given_mu, dim = 0)
 numerical_posterior = nonnormalized_posterior/(likelihood_given_mu.repeat([1,n_disc,1]) * 6/ n_disc)
 
 max_index_mu = torch.argmax(product_likelihoods_given_mu)
-mu_bar_graphical = mu_disc[max_index_mu]
+mu_bias_numerical = mu_disc[max_index_mu]
 
 posterior_z = numerical_posterior[:, :, max_index_mu]
 max_index_z = torch.argmax(posterior_z, 1)
 z_map_graphical = z_disc[max_index_z]
 
 
-# # iii) Compute closed form solution of posterior
 
-# data_mean = torch.mean(data)
-# var_post = torch.tensor(1/((n_data/sigma_T**2)+(1/sigma_z**2)))
-# mu_post = var_post * ((mu_z/sigma_z**2) + n_data * (data_mean/sigma_T**2))
-
-# def posterior(z):
-#     coeff_1 = (1/(torch.sqrt(var_post)*torch.sqrt(2*torch.tensor(torch.pi))))
-#     coeff_2 = -(1/(2*var_post))
-#     fun_val = coeff_1 * torch.exp( coeff_2 * (z - mu_post)**2)
-#     return fun_val
-
-# analytical_posterior = torch.zeros([n_disc])
-# for k in range(n_disc):
-#     analytical_posterior[k] = posterior(z_disc[k])
+"""
+    4. Pyro model and inference
+"""
 
 
+# i) Pyro model
 
-# """
-#     4. Pyro model and inference
-# """
+def model(observations = None):    
+    # Define unknown bias
+    bias_mu = pyro.param('bias_mu', torch.zeros([1,1]))
+    
+    # Sample from latent and observation distribution
+    dist_z = pyro.distributions.Normal(mu_z, sigma_z).expand([n_epochs,1])
+    with pyro.plate('z_plate', size = n_epochs, dim = -2):
+        
+        # Sample from latent variable to determine observation distribution
+        z = pyro.sample('latent_z', dist_z)
+        dist_obs = pyro.distributions.Normal(z + bias_mu, sigma_T)
+        dist_obs_reshaped = dist_obs.expand([n_epochs,n_data])
+        
+        with pyro.plate('measure_plate', size = n_data, dim = -1):
+            # Sample from observation distribution
+            sample = pyro.sample('sample', dist_obs_reshaped, obs = observations)
+    return sample, z
 
+
+# ii) Pyro guide
+
+def guide(observations = None):
+    # Define posterior distribution
+    mu_post = pyro.param('mu_post', init_tensor = torch.zeros([n_epochs,1]))
+    sigma_post = pyro.param('sigma_post', init_tensor = torch.ones([n_epochs,1]))
+    
+    # Sample from posterior distribution
+    dist_post = pyro.distributions.Normal(mu_post, sigma_post)
+    with pyro.plate('z_plate', size = n_epochs, dim = -2):
+        z_post = pyro.sample('latent_z', dist_post)
+
+    return z_post
+
+
+# # Alternative formulation for the model and guide. Difference: Use of pyro.plate
+# # as declarator and constructor. This allows easier dimensions for z, obs dists
 
 # # i) Pyro model
 
-# def model(observations = None):
+# def model(observations = None):    
+#     # Define unknown bias
+#     bias_mu = pyro.param('bias_mu', torch.zeros([1]))
+    
 #     # Sample from latent variable to determine observation distribution
 #     dist_z = pyro.distributions.Normal(mu_z, sigma_z)
-#     z = pyro.sample('latent_z', dist_z)
-#     dist_obs = pyro.distributions.Normal(z, sigma_T)
+
     
 #     # Sample from observation distribution
-#     with pyro.plate('batch_plate', size = n_data, dim = -1):
-#         sample = pyro.sample('sample', dist_obs, obs = observations)
-#     return sample
+#     with pyro.plate('z_plate', size = n_epochs, dim = -2):
+#         z = pyro.sample('latent_z', dist_z)
+#         dist_obs = pyro.distributions.Normal(z + bias_mu, sigma_T)
+        
+#         with pyro.plate('measure_plate', size = n_data, dim = -1):
+#                 sample = pyro.sample('sample', dist_obs, obs = observations)
+                
+#     return sample, z
 
 
 # # ii) Pyro guide
 
-# # def guide(observations = None):
-# #     pass
-
 # def guide(observations = None):
 #     # Define posterior distribution
-#     mu_post = pyro.param('mu_post', init_tensor = torch.zeros([1]))
-#     sigma_post = pyro.param('sigma_post', init_tensor = torch.ones([1]))
+#     mu_post = pyro.param('mu_post', init_tensor = torch.zeros([n_epochs,1]))
+#     sigma_post = pyro.param('sigma_post', init_tensor = torch.ones([n_epochs,1]),
+#                             constraint = pyro.distributions.constraints.positive)
     
-#     dist_post = pyro.distributions.Normal(mu_post, sigma_post)
-#     z_post = pyro.sample('latent_z', dist_post)
+#     # Sample from posterior distribution
+#     with pyro.plate('z_plate', size = n_epochs, dim = -2):
+#         dist_post = pyro.distributions.Normal(mu_post, sigma_post)
+#         z_post = pyro.sample('latent_z', dist_post)
 
 #     return z_post
 
 
-# # iii) Pyro inference
+# iii) Pyro diagnostics
 
-# adam = pyro.optim.NAdam({"lr": 0.1})
-# elbo = pyro.infer.Trace_ELBO()
-# svi = pyro.infer.SVI(model, guide, adam, elbo)
+# Render model and guide
+pyro.render_model(model, model_args=(), render_distributions=True, render_params=True)  
+pyro.render_model(guide, model_args=(), render_distributions=True, render_params=True)  
 
-# loss_sequence = []
-# mu_post_sequence = []
-# sigma_post_sequence = []
-# for step in range(1000):
-#     loss = svi.step(data)
-#     if step % 100 == 0:
-#         print('epoch: {} ; loss : {}'.format(step, loss))
-#     else:
-#         pass
-#     loss_sequence.append(loss)
-#     mu_post_sequence.append(pyro.get_param_store()['mu_post'].item())
-#     sigma_post_sequence.append(pyro.get_param_store()['sigma_post'].item())
+# Showcase the execution traces
+model_trace_data = pyro.poutine.trace(model).get_trace(data)
+model_trace_data.compute_log_prob()
+print(model_trace_data.format_shapes())
+
+guide_trace_data = pyro.poutine.trace(guide).get_trace(data)
+guide_trace_data.compute_log_prob()
+print(guide_trace_data.format_shapes())
+
+
+
+# iii) Pyro inference
+
+adam = pyro.optim.NAdam({"lr": 0.05})
+elbo = pyro.infer.Trace_ELBO()
+svi = pyro.infer.SVI(model, guide, adam, elbo)
+
+loss_sequence = []
+mu_bias_sequence = []
+mu_post_sequence = []
+sigma_post_sequence = []
+for step in range(5000):
+    loss = svi.step(data)
+    if step % 500 == 0:
+        print('epoch: {} ; loss : {}'.format(step, loss))
+    else:
+        pass
+    loss_sequence.append(loss)
+    mu_bias_sequence.append(pyro.get_param_store()['bias_mu'].detach().numpy().flatten())
+    mu_post_sequence.append(pyro.get_param_store()['mu_post'].detach().numpy().flatten())
+    sigma_post_sequence.append(pyro.get_param_store()['sigma_post'].detach().numpy().flatten())
 
 
 
@@ -271,26 +319,72 @@ plt.tight_layout()
 plt.show()
 
 
-# iii) Data & posterior densities of z
+# iii) Training process
 
-fig, ax = plt.subplots(3,1, figsize = (10,15))
-ax[0].plot(z_disc, posterior_z[0,:])
-ax[0].hist(data[0,:].numpy(), density = True)
-ax[0].stem(z_true[0].numpy(),[1])
+fig, ax = plt.subplots(1,4, figsize = (15,5))
+ax[0].plot(loss_sequence)
+ax[0].set_title('Loss during computation')
+
+ax[1].plot(mu_bias_sequence)
+ax[1].set_title('Estimated bias during computation')
+
+ax[2].plot(mu_post_sequence)
+ax[2].set_title('Estimated posterior mu during computation')
+
+ax[3].plot(sigma_post_sequence)
+ax[3].set_title('Estimated posterior sigma during computation')
+
+plt.tight_layout()
+plt.show()
+
+
+# iv) Print results
+
+mu_bias_pyro = pyro.get_param_store()['bias_mu'].detach().numpy().flatten()
+mu_post_pyro = pyro.get_param_store()['mu_post'].detach().numpy().flatten()
+sigma_post_pyro = pyro.get_param_store()['sigma_post'].detach().numpy().flatten()
+print(' True bias = {} \n bias_numerical = {} \n bias_pyro = {}'
+      .format(mu_bias, mu_bias_numerical, mu_bias_pyro))
+# print(' True z = {} \n true posterior mu = {} \n pyro posterior mu = {}'\
+#       ' \n true posterior sigma = {} \n pyro posterior sigma = {}'
+#       .format(z_true, mu_post, mu_post_pyro, torch.sqrt(var_post), sigma_post_pyro))
+
+
+# v) Data & posterior densities of z
+
+def pyro_posterior(z):
+    coeff_1 = (1/(torch.tensor(sigma_post_pyro)*torch.sqrt(2*torch.tensor(torch.pi))))
+    coeff_2 = -(1/(2*torch.tensor(sigma_post_pyro)**2))
+    fun_vals = coeff_1 * torch.exp( coeff_2 * (z - mu_post_pyro)**2)
+    return fun_vals
+
+pyro_posterior_z = torch.zeros([n_epochs, n_disc])
+for k in range(n_disc):
+    prob_vals = pyro_posterior(z_disc[k])
+    pyro_posterior_z[:,k] = prob_vals.flatten()
+
+fig, ax = plt.subplots(3,1, figsize = (10,10))
+ax[0].plot(z_disc, posterior_z[0,:], label = 'true posterior')
+ax[0].plot(z_disc, pyro_posterior_z[0,:], label = 'pyro posterior')
+ax[0].hist(data[0,:].numpy(), density = True, label = 'data')
+ax[0].stem(z_true[0].numpy(),[1], label = 'true z')
 ax[0].set_title('Data & posterior over z first epoch')
 ax[0].set_xlabel('z')
 ax[0].set_ylabel('probability')
+ax[0].legend()
 
-ax[1].plot(z_disc, posterior_z[1,:])
-ax[1].hist(data[1,:].numpy(), density = True)
-ax[1].stem(z_true[1].numpy(),[1])
+ax[1].plot(z_disc, posterior_z[1,:], label = 'true posterior')
+ax[1].plot(z_disc, pyro_posterior_z[1,:], label = 'pyro posterior')
+ax[1].hist(data[1,:].numpy(), density = True, label = 'data')
+ax[1].stem(z_true[1].numpy(),[1], label = 'true z')
 ax[1].set_title('Data & posterior over z second epoch')
 ax[1].set_xlabel('z')
 ax[1].set_ylabel('probability')
 
-ax[2].plot(z_disc, posterior_z[2,:])
-ax[2].hist(data[2,:].numpy(), density = True)
-ax[2].stem(z_true[2].numpy(),[1])
+ax[2].plot(z_disc, posterior_z[2,:], label = 'true posterior')
+ax[2].plot(z_disc, pyro_posterior_z[2,:], label = 'pyro posterior')
+ax[2].hist(data[2,:].numpy(), density = True, label = 'data')
+ax[2].stem(z_true[2].numpy(),[1], label = 'true z')
 ax[2].set_title('Data & posterior over z third epoch')
 ax[2].set_xlabel('z')
 ax[2].set_ylabel('probability')
@@ -299,24 +393,8 @@ plt.tight_layout()
 plt.show()
 
 
-# ii) Training process
-
-fig, ax = plt.subplots(1,3, figsize = (10,5))
-ax[0].plot(loss_sequence)
-ax[0].set_title('Loss during computation')
-
-ax[1].plot(mu_post_sequence)
-ax[1].set_title('Estimated posterior mu during computation')
-
-ax[2].plot(sigma_post_sequence)
-ax[2].set_title('Estimated posterior sigma during computation')
 
 
-# iii) Print results
 
-mu_post_pyro = pyro.get_param_store()['mu_post'].item()
-sigma_post_pyro = pyro.get_param_store()['sigma_post'].item()
-print(' True z = {} \n true posterior mu = {} \n pyro posterior mu = {}'\
-      ' \n true posterior sigma = {} \n pyro posterior sigma = {}'
-      .format(z_true, mu_post, mu_post_pyro, torch.sqrt(var_post), sigma_post_pyro))
+
 
