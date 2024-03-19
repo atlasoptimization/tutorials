ea3d815550f53e94cb9ccf3334564d6532205530
atlasoptimization <jemilbutt.opt@gmail.com>
2024-03-19 20:15:08 +0100

    Add training to pytorch_tutorial_task_1.py

--- pytorch/pytorch_tutorial_task_1.py
+++ pytorch/pytorch_tutorial_task_1.py
@@ -39,7 +39,8 @@ import matplotlib.pyplot as plt
 # ii) Definitions
 
 n_time = 100
-n_data = 10
+n_data = 100
+n_classes = 2
 time = torch.linspace(0,1,n_time)
 
 
@@ -72,11 +73,20 @@ for k in range(n_data):
     labels[k,0] = random_slope >=0
     timeseries[k,:] = random_slope * time + random_noise
 
+# Create a reshaped version of the labels containing two columns with a 1 in 
+# the first column indicating a label of 0 and 1 in the second column indicating
+# a label of 1. This labels_reshaped matrix can be compared against the class
+# probabilities that the ANN you want to train will output.
+labels_reshaped = torch.hstack((1.0*(labels == 0 ), 1.0*(labels == 1)))
 
-# ii) Classify timeseries
-
-
-
+# Separate into train and test set
+# Split the dataset into training and testing sets
+train_size = int(round(0.8 * n_data))           # 80% of the data for training
+test_size = n_data - train_size                 # 20% of the data for testing
+timeseries_train = timeseries[0:train_size, :]
+labels_reshaped_train = labels_reshaped[0:train_size, :]
+timeseries_test = timeseries[train_size :, :]
+labels_reshaped_test = labels_reshaped[train_size :, :]
 
 # The rule for classifying the timeseries is not available in practice and we 
 # employ it here only to have some simple synthetic dataset that we fully understand.
@@ -89,57 +99,66 @@ for k in range(n_data):
 
 
 
-
 """
-    3. Auxiliary definitions VAE
-"""
-
-
-# i) ANN class
-
-class ANN(pyro.nn.PyroModule):
-    # Initialize by invoking base class
-    def __init__(self, dim_input, dim_hidden, dim_output):
-        super().__init__()
-        
-        # Integration basic data
-        self.dim_input = dim_input
-        self.dim_hidden = dim_hidden
-        self.dim_output = dim_output
-        
-        # Linear transforms
-        self.fc_1 = torch.nn.Linear(self.dim_input, self.dim_hidden)
-        self.fc_2 = torch.nn.Linear(self.dim_hidden, self.dim_hidden)
-        self.fc_3 = torch.nn.Linear(self.dim_hidden, self.dim_output)
-        # nonlinear transforms
-        self.nonlinear = torch.nn.ReLU()
-        
-    def forward(self, x):
-        # Define forward computation on the input data x
-        # Shape the minibatch so that batch_dims are on left, argument_dims on right
-        x = x.reshape([-1, self.dim_input])
-        
-        # Then compute hidden units and output of nonlinear pass
-        hidden_units_1 = self.nonlinear(self.fc_1(x))
-        hidden_units_2 = self.nonlinear(self.fc_2(hidden_units_1))
-        output = self.fc_3(hidden_units_2)
-
-        return output
-
-
-
-
-
-"""
-    4. Set up entire VAE
+    3. Setup neural network class       <-- your task
 """
 
+# i) Define ANN class
+
+# To do this, you need to define a class that inherits from the neural network
+# base class of torch; this is torch.nn.Module. The ANN class should be built
+# using info on the dimensionality of the input, the hidden dimension, and the
+# output. Try to come up with a simple ANN that maps full timeseries to the
+# hidden layers and then into an output of dim [n_data, 2] where the second 
+# dimension records the class probabilities of the labels for each datapoint.
+# An output[0,:] = [0.9, 0.1] would therefore be interpreted as the ANN predicting
+# for the first timeseries a label of zero with certainty 0.9 and a label of one
+# with certainty 0.1.
+
+
+# Now you need to come up with some code yourself. The comments are meant to give
+# some guiderails in constructing the ANN class.
+
+# Write your own stuff starting from here : ----------------------------------
+
+# Define ANN class
+# 1. Define your class name and inheritance
+# 2. Initialize the class
+# 3. Define the layers
+# 4. Define the result of the forward pass
+#
+# The result will look a bit like the following blueprint that you need to adapt"
+# class ANN(ClassToInheritFrom):
+#       def __init__(self, your inputs):
+#           super().__init__()
+#           self.some_attribute = some_input
+#
+#           self.linear_layer_1 = torch.nn.Linear(dim_layer_input, dim_layer_output)
+#           self.nonlinearity = torch.nn.Sigmoid()
+#
+#       def forward(self, x):
+#           x = x.reshape([-1,appropriate_length])
+#           hidden_1 = self.nonlinearity(self.linear_layer_1(x))
+#           hidden_2 = ...
+#           class_probs = self.nonlinearity(hidden_2)
+
+
+# Then you need to instantiate one neural network with appropriate dimensions
+# by calling the class with some input arguments.
+
+
+# You can now stop writing your own stuff for section 3. ---------------------
+
 
 
 """
-    5. Perform inference
+    4. Train neural network             <-- your task
 """
 
+
+"""
+    5. Investigate activation triggers  <-- your task
+"""
 
 
 
@@ -148,91 +167,114 @@ class ANN(pyro.nn.PyroModule):
 """
 
 
-# i) Illustrate model and guide 
-# (execute following lines one by one in console to render)
 
-pyro.render_model(vae.model, model_args=(data,), render_distributions=True, render_params=True)
-pyro.render_model(vae.guide, model_args=(data,), render_distributions=True, render_params=True)
-
-
-# ii) Plot training progress
-
-fig = plt.figure(1, figsize = (10,5))
-plt.plot(loss_sequence)
-plt.title('Loss during computation')
+""" 
+    7. Exemplary solution ----------------------------------------------------
+"""
 
 
-# iii) Showcase data
+# For section 3: Define the neural network class and invoke an instance. 
 
-fig, ax = plt.subplots(3,3, figsize = (10,10), dpi = 300)
-ax[0,0].imshow(data[0,:].reshape([n_space,n_space]))
-ax[0,0].set_title('Original data - type 1')
-ax[0,1].imshow(data[1,:].reshape([n_space,n_space]))
-ax[0,2].imshow(data[2,:].reshape([n_space,n_space]))
+# Neural network class
+class ANN(torch.nn.Module):
+    def __init__(self, dim_input, dim_hidden, dim_output):
+        super().__init__()
+        # Remember: The  __init__() method defines how a class is initialized.
+        # Here we declare that the first step of class initialization consists
+        # in initializing the class we are inheriting from (torch.nn.Module),
+        # the archetypical class from which all ANN's are just specific instances.
+        # This is what the super.__init__() statement means - > Initialize the
+        # superclass of this subclass.
+        
+        # Integrate basic data into ANN
+        self.dim_input = dim_input
+        self.dim_hidden = dim_hidden
+        self.dim_output = dim_output
+        
+        # Lets build a transformation that maps timeseries into two class 
+        # probabilities. Specify two linear layers and a nonlinearity for that.
+        # First, two fully connected linear layers via torch.nn.Linear
+        self.fc1 = torch.nn.Linear(dim_input, dim_hidden)
+        self.fc2 = torch.nn.Linear(dim_hidden, dim_output)
+        
+        # Second, setup the sigmoid function as non-linearity
+        self.sigmoid = torch.nn.Sigmoid()
 
-ax[1,0].imshow(data[n_data,:].reshape([n_space,n_space]))
-ax[1,0].set_title('Original data - type 2')
-ax[1,1].imshow(data[n_data+1,:].reshape([n_space,n_space]))
-ax[1,2].imshow(data[n_data+2,:].reshape([n_space,n_space]))
+    def forward(self, x):
+        # define the forward computation on the input data x
+        # first, reshape to have batch dimension on the left
+        x = x.reshape([-1,self.dim_input])
+        # second, compute hidden layer
+        hidden = self.sigmoid(self.fc1(x))
+        # third, transform from hidden to output and return
+        class_probs = self.sigmoid(self.fc2(hidden))
+        return class_probs
 
-ax[2,0].imshow(data[2*n_data,:].reshape([n_space,n_space]))
-ax[2,0].set_title('Original data - type 3')
-ax[2,1].imshow(data[2*n_data+1,:].reshape([n_space,n_space]))
-ax[2,2].imshow(data[2*n_data+2,:].reshape([n_space,n_space]))
+# Invoke an instance
+classification_ann = ANN(n_time, 2, n_classes)
 
-plt.tight_layout()
-plt.show()
-
-
-# iv) Showcase trained and untrained resimulations
-
-fig, ax = plt.subplots(3,7, figsize = (15,5), dpi = 300)
-# first row, type I
-ax[0,0].imshow(data[0,:].reshape([n_space,n_space]))
-ax[0,0].set_title('Original data - type 1')
-ax[0,1].imshow(images_resimu_untrained_type1[0,:,:])
-ax[0,1].set_title('Resimulations untrained')
-ax[0,2].imshow(images_resimu_untrained_type1[1,:,:])
-ax[0,3].imshow(images_resimu_untrained_type1[2,:,:])
-ax[0,4].imshow(images_resimu_trained_type1[0,:,:])
-ax[0,4].set_title('Resimulations trained')
-ax[0,5].imshow(images_resimu_trained_type1[1,:,:])
-ax[0,6].imshow(images_resimu_trained_type1[2,:,:])
-
-# second row, type II
-ax[1,0].imshow(data[n_data,:].reshape([n_space,n_space]))
-ax[1,0].set_title('Original data - type 2')
-ax[1,1].imshow(images_resimu_untrained_type2[0,:,:])
-ax[1,1].set_title('Resimulations untrained')
-ax[1,2].imshow(images_resimu_untrained_type2[1,:,:])
-ax[1,3].imshow(images_resimu_untrained_type2[2,:,:])
-ax[1,4].imshow(images_resimu_trained_type2[0,:,:])
-ax[1,4].set_title('Resimulations trained')
-ax[1,5].imshow(images_resimu_trained_type2[1,:,:])
-ax[1,6].imshow(images_resimu_trained_type2[2,:,:])
-
-# third row, type III
-ax[2,0].imshow(data[2*n_data,:].reshape([n_space,n_space]))
-ax[2,0].set_title('Original data - type 3')
-ax[2,1].imshow(images_resimu_untrained_type3[0,:,:])
-ax[2,1].set_title('Resimulations untrained')
-ax[2,2].imshow(images_resimu_untrained_type3[1,:,:])
-ax[2,3].imshow(images_resimu_untrained_type3[2,:,:])
-ax[2,4].imshow(images_resimu_trained_type3[0,:,:])
-ax[2,4].set_title('Resimulations trained')
-ax[2,5].imshow(images_resimu_trained_type3[1,:,:])
-ax[2,6].imshow(images_resimu_trained_type3[2,:,:])
-
-plt.tight_layout()
-plt.show()
+# You can investigate this ann by printing it out and looking at its attributes 
+# and methods, e.g. you can show the architecture and the parameters by calling
+# classification_ann or classification_ann.state_dict(), respectively.
 
 
-# vi) Showcase distribution of hidden variables
+# For section 4: Set up optimization and train classification_ann
 
-z_loc, z_scale = vae.encoder(data)[:,0:dim_bottleneck].detach().numpy(),\
-        vae.encoder(data)[:,dim_bottleneck : 2*dim_bottleneck].detach().numpy()
-fig, ax = plt.subplots(2,1, figsize = (10,10), dpi = 300)
-ax[0].hist(z_loc)
-ax[0].set_title('Distribution of latent variables; colors indicating dimension')
-ax[1].scatter(z_loc[:,0], z_loc[:,1])
-ax[1].set_title('Scatterplot of latent variables')
+# Define the optimizer
+# We choose Adam as optimizer; it is a reliable standard choice for ANN training
+# We set the learning rate to 0.001 corresponding to 1/100th of a gradient step
+# for each iteration in the training loop and declare the ann parameters as the
+# subjects of optimization.
+optimizer = torch.optim.Adam(classification_ann.parameters(), lr=0.01) 
+
+# Define the loss 
+# We choose binary cross entropy as loss; it is a standard loss for binary 
+# classification tasks
+loss_fun = torch.nn.BCELoss()
+loss_history = []
+
+
+# ii) Optimize
+
+for step in range(1000):
+    # Set the gradients to zero before starting to do backpropragation because 
+    # pytorch accumulates the gradients on subsequent backward passes.
+    optimizer.zero_grad()  
+    
+    # compute the loss function
+    loss = loss_fun(classification_ann(timeseries_train), labels_reshaped_train)
+    
+    # compute the gradients
+    loss.backward()
+    # update the weights, record new parameters and loss
+    optimizer.step()
+    loss_history.append(loss.item())
+    
+    # print the loss value and the value of x at specifix steps
+    if step % 100 == 0:
+        print(f"Step {step+1}, Loss {loss.item()}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
